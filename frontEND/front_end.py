44adba737ac0b60027cca6d57c2cdaae8f7b1e25
SpAs3j9R <jekatopgg@gmail>
2024-11-19 17:50:01 +0300

    damn

--- frontEND/front_end.py
+++ frontEND/front_end.py
@@ -1,14 +1,19 @@
 import sys
-from PyQt6.QtWidgets import QApplication, QWidget
+from PyQt6 import uic
+from PyQt6.QtWidgets import QApplication, QWidget, QPushButton
 
 
 class Minesweeper(QWidget):
     def __init__(self):
         super().__init__()
+        uic.loadUi("minesweeper.ui", self)
         self.init_ui()
+        self.board = []
 
     def init_ui(self):
-        pass
+        for i in range(10):
+            for j in range(10):
+                self.board[i][j] = QPushButton(self)
 
 
 if __name__ == '__main__':
