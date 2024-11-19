e40dfa672c0bdf8517c4d754e59b896d1bc90fef
FrogCpp <161460644+FrogCpp@users.noreply.github.com>
2024-11-19 17:49:18 +0300

    Cells

--- SaperClasses/BoardClasses.py
+++ SaperClasses/BoardClasses.py
@@ -1,3 +1,22 @@
-class Board():
-    def __init__(self):
-        pass
+from .CellClass import Cell
+
+class Board:
+    def __init__(self, With : int, Hight : int):
+        self.Board = []
+        for i in range(With):
+            help = []
+            for j in range(Hight):
+                help.append(Cell(Position=(i, j)))
+            self.Board.append(help)
+        for lines in self.Board:
+            for point in lines:
+                mass = []
+                for i in range(-1, 2):
+                    help = []
+                    for j in range(-1, 2):
+                        try:
+                            help.append(self.Board[point.Position[0] + i][point.Position[1] + j])
+                        except Exception:
+                            help.append(None)
+                    mass.append(help)
+                point.SetMyFriends(mass)
