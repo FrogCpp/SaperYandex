2b213cc97a77a953f422b8cef5adc820cc45b363
FrogCpp <161460644+FrogCpp@users.noreply.github.com>
2024-12-03 19:00:51 +0300

    zalupa

--- HubMenu/FrontEnd/MainWindow.py
+++ HubMenu/FrontEnd/MainWindow.py
@@ -20,6 +20,8 @@ class MainWindowClass(QMainWindow):
         self.first_v = [0, 0]
         self.second_v = [0, 0]
         self.ferd_v = [0, 0]
+        self.forth_v = [0, 0]
+        self.fifth_v = [0, 0]
         self.Refresh()
 
     def Refresh(self):
@@ -27,10 +29,14 @@ class MainWindowClass(QMainWindow):
         self.first_v = ab[0] if len(ab) > 0 else ['None', 'None']
         self.second_v = ab[1] if len(ab) > 1 else ['None', 'None']
         self.ferd_v = ab[2] if len(ab) > 2 else ['None', 'None']
+        self.forth_v = ab[3] if len(ab) > 3 else ['None', 'None']
+        self.fifth_v = ab[4] if len(ab) > 4 else ['None', 'None']
         self.first.setText(f"1:\ntime:{self.first_v[0]}, Date:{self.first_v[1]}")
         self.second.setText(f"2:\ntime:{self.second_v[0]}, Date:{self.second_v[1]}")
         self.ferd.setText(f"3:\ntime:{self.ferd_v[0]}, Date:{self.ferd_v[1]}")
+        self.forth.setText(f"4:\ntime:{self.forth_v[0]}, Date:{self.forth_v[1]}")
+        self.fifth.setText(f"5:\ntime:{self.fifth_v[0]}, Date:{self.fifth_v[1]}")
 
         ab =list(sorted(map(lambda x: (int(x[1]), x[2], x[0]), self.CS.drawTable(True)['bestTime'])))
-        for i in ab[3:]:
+        for i in ab[5:]:
             self.CS.Delet(i[2])
